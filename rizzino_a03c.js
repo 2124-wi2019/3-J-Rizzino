93ce751070831de0d43ce9460b8fce18a1fcbf2d
J-Rizzino <58890452+J-Rizzino@users.noreply.github.com>
2019-12-22 10:09:17 -0600

    adding rizzino_a03b.js and rizzino_a03c.js
    Adding task 1 and 2 for assignment03.  All parts have been revised and should be working.

---
+++ rizzino_a03c.js
@@ -0,0 +1,13 @@
+/* Julie Rizzino
+rizzino_a03c.js
+INFO 2124
+Thoendel
+12/17/19 */
+let preamble = `When in the course of human events`;
+let message = 
+`The preamble is: 
+\t${preamble}
+
+Does the preamble contain 'a'?
+\t${preamble.includes('a')}`;
+console.log(message);
